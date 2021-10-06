5bdb87ab63dc0daacd53d1d5c1cec745aa2f178d
Jahangir Alam <ja.dakghor@gmail.com>
2021-10-06 14:04:33 +0600

    retrive value from application.properties
    Signed-off-by: Jahangir Alam <ja.dakghor@gmail.com>

--- limits-service/src/main/java/com/sangkhochil/microservices/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/sangkhochil/microservices/limitsservice/controller/LimitsController.java
@@ -1,14 +1,19 @@
 package com.sangkhochil.microservices.limitsservice.controller;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sangkhochil.microservices.limitsservice.Configuration.Configuration;
 import com.sangkhochil.microservices.limitsservice.bean.Limits;
 
 @RestController
 public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping(path = "/limits")
 	public Limits retriveLimits() {
-		return new Limits(1, 1000);
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
 	}
 }
