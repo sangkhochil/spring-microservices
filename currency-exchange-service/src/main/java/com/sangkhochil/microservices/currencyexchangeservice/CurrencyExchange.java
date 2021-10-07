3159fda09ae5faf4e011e8609da4ee3673b06e13
Jahangir Alam <ja.dakghor@gmail.com>
2021-10-07 09:19:50 +0600

    configure jpa and h2 database
    Signed-off-by: Jahangir Alam <ja.dakghor@gmail.com>

--- currency-exchange-service/src/main/java/com/sangkhochil/microservices/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/sangkhochil/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -2,9 +2,17 @@ package com.sangkhochil.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class CurrencyExchange {
+	@Id
 	private Long id;
+	@Column(name = "currency_from")
 	private String from;
+	@Column(name = "currency_to")
 	private String to;
 	private BigDecimal exchangeRate;
 	private String environment;
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into currency_exchange(id,currency_from,currency_to,exchange_rate,environment) values(1001, 'USD', 'BDT', 85 ,'');
+insert into currency_exchange(id,currency_from,currency_to,exchange_rate,environment) values(1002, 'EUR', 'BDT', 99 ,'');
+insert into currency_exchange(id,currency_from,currency_to,exchange_rate,environment) values(1003, 'AUD', 'BDT', 60 ,'');
