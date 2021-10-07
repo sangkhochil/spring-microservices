f5ef1712f8f6b08a7493e47b284888b1853aefc2
Jahangir Alam <ja.dakghor@gmail.com>
2021-10-07 10:20:54 +0600

    add jpa repository and retrive from h2 database
    Signed-off-by: Jahangir Alam <ja.dakghor@gmail.com>

--- currency-exchange-service/src/main/java/com/sangkhochil/microservices/currencyexchangeservice/CurrencyExchangerController.java
+++ currency-exchange-service/src/main/java/com/sangkhochil/microservices/currencyexchangeservice/CurrencyExchangerController.java
@@ -1,7 +1,5 @@
 package com.sangkhochil.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangerController {
+	@Autowired
+	CurrencyExchangeRepository reposity;
 	
 	@Autowired
 	private Environment environment;
 	
 	@GetMapping(path = "/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		CurrencyExchange currencyExchange = new CurrencyExchange(1000L,from, to, BigDecimal.valueOf(85.909));
+//		CurrencyExchange currencyExchange = new CurrencyExchange(1000L,from, to, BigDecimal.valueOf(85.909));
+		
+		CurrencyExchange currencyExchange = reposity.findByFromAndTo(from, to);
+		if(currencyExchange == null)
+			throw new RuntimeException(String.format("Unable to retrive convertion rate for %s to %s",from, to));
 		
 		currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
 		return currencyExchange;
