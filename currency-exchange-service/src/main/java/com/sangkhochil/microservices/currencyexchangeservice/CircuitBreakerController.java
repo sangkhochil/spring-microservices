66725b0afc9e81995cf25ad01536a23f98ea6e1b
Jahangir Alam <ja.dakghor@gmail.com>
2021-10-08 17:10:02 +0600

    implement retry with number of time and fallback response
    Signed-off-by: Jahangir Alam <ja.dakghor@gmail.com>

--- currency-exchange-service/src/main/java/com/sangkhochil/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/sangkhochil/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,13 +1,28 @@
 package com.sangkhochil.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuitBreakerController {
 	
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+	
 	@GetMapping(path="/sample-api")
+	@Retry(name = "sample-api", fallbackMethod = "hardCodedresponse")
 	public String sampleApi() {
-		return "Sample API";
+		logger.info("sample-api call received");
+		ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/some-dummy", String.class);
+		return response.getBody();
+	}
+	
+	public String hardCodedresponse(Exception ex) {
+		return "fallback response";
 	}
 }
